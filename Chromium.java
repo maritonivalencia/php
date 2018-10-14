ad4e7334b0b7ee3c3f7bbf1f4a3002dc54382d95
Catherine Ravenhill <44057355+chromiumcat@users.noreply.github.com>
2018-10-14 22:01:50 +0100

    Update Chromium.java
    Made an overly-complicated loop.

--- Chromium.java
+++ Chromium.java
@@ -3,6 +3,23 @@ public class Chromium {
     public static void main(String[] args) {
   
         System.out.println("Hello, World");
+        
+        boolean doContinue = true;
+        int continueCount = 0;
+        
+        while(doContinue) {
+            
+           if(continueCount < 10) {
+               
+               doContinue = false;
+               
+           }
+            
+           System.out.println("Loop de loop");
+            
+           continueCount++;
+            
+        }
   
     }
   
